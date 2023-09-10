14a286dfdb23ec4e172b744aa76f5400b6ba8a95
William East <wjbetech@gmail.com>
2023-09-10 18:13:52 +0900

    Modal code started but incomplete

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import "./Meal.css"
+import { useState } from 'react'
+import { Modal } from './Modal'
 
 export const Meal = ({ meal }) => {
 
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleClick = () => {
+        setOpenModal(!openModal);
+    }
+
     return (
         <div className="single-meal">
-            <img 
-                className='single-meal-img' 
-                src={meal.strMealThumb} 
-                alt={meal.strMealThumb} />
-            <h3>{meal.strMeal}</h3>
+            <div 
+                onClick={handleClick}
+            >
+                <img 
+                    className='single-meal-img' 
+                    src={meal.strMealThumb} 
+                    alt={meal.strMealThumb}
+                    />
+                <h3>{meal.strMeal}</h3>
+            </div>
+            <div className="modal-container">
+                {openModal && (
+                    <Modal meal={meal} />
+                )}
+            </div>
         </div>
     )
 }
