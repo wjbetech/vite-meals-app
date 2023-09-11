55805d9f23375dc2cb9e6e944314a2621791ccee
William East <wjbetech@gmail.com>
2023-09-11 20:44:59 +0900

    Fixed up functionality of input

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import logo from "../assets/wjbetech-logo-transparent.png"
 import "./navbar.css"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export const NavBar = ({ onMealsLoaded, meals }) => {
 
     // state
-    const [search, setSearch] = useState("");
+    const [initialSearch, setInitialSearch] = useState("chicken");
+    const [search, setSearch] = useState("chicken");
 
     // functionaltiy
+    const firstMealSearch = () => {
+        axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`)
+            .then((res) => {
+                const mealsData = res.data.meals;
+                onMealsLoaded(mealsData);
+            })
+            .catch((err) => {
+                console.error("Error!", err)
+            });
+    };
+
     const handleChange = (event) => {
         setSearch(event.target.value)
     }
@@ -28,6 +40,21 @@ export const NavBar = ({ onMealsLoaded, meals }) => {
         console.log(meals)
     }
 
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            firstMealSearch();
+        }
+    }
+
+    // useEffect
+    useEffect(() => {
+        if(initialSearch) {
+            firstMealSearch();
+            setInitialSearch(false);
+        }
+    }, [search]);
+
   return (
     <div className="navbar">
         <div className="nav-left-container">
@@ -43,6 +70,7 @@ export const NavBar = ({ onMealsLoaded, meals }) => {
                     placeholder="Search an ingredient.."
                     value={search}
                     onChange={handleChange}
+                    onKeyDown={handleKeyDown}
                 />
                 <button
                     type="button"
